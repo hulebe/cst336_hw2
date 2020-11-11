d0b6c8707681092173f413dc5043043eaf3f4d3c
EC2 Default User <ec2-user@ip-172-31-24-95.us-west-1.compute.internal>
2020-11-11 07:49:45 +0000

    update

---
+++ js/functions.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    
+    // variables
+    var winnings = 0;
+    var total = 0;
+    var bet = 0;
+    var slotArray = ["cherry", "seven", "bar"];
+    
+    $("button").on("click", spin); 
+    
+    function spin() {
+        bet = $("#betField").val();
+        let slots = 3;
+        
+        if (bet < 1 || bet > 100){
+            $("#warning").text("Invalid bet");
+        }
+        
+        let s1 = Math.floor(Math.random() * slots) + 1;
+        let s2 = Math.floor(Math.random() * slots) + 1;
+        let s3 = Math.floor(Math.random() * slots) + 1;
+        
+        $("#p1").attr("src", `img/img${s1}.jpeg`);
+        $("#p2").attr("src", `img/img${s2}.jpeg`);
+        $("#p3").attr("src", `img/img${s3}.jpeg`);  
+        
+        if (s1 == s2 && s1 == s3) {
+            bet = bet * 10;
+            total += bet;
+            $("#winnings").html("Congrats, you trippled: " + bet);
+            $("#total").html("Total winnings: " + total);
+        } else if (s1 == s2 || s2 == s3 || s3 == s1) {
+            bet = bet * 5;
+            total += bet;
+            $("#winnings").html("Congrats, you doubled your bet: " + bet);
+            $("#total").html("Total winnings: " + total);
+        } else {
+            $("#winnings").html("Don't give up");
+        }
+        
+    }
+    
+})
